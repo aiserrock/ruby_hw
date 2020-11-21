cf1ccdd13ab2d461c60466159ce7971ad0dbb64e
aiserrock <aicserrock@gmail.com>
2020-11-21 20:06:06 +0300

    complited calculat.rb

---
+++ hw2/calculate.rb
@@ -0,0 +1,26 @@
+def main
+  amount = 0.0
+  puts 'I am working!'
+  puts "My arguments are: #{ARGV}"
+  puts "Приложение калькулятор, складывает вещественные числа.Для выхода напишите over"
+  while true
+    print "Введите данные >"
+    data = gets
+    break unless data
+    if data == "over"
+      break
+    end
+    data = Float(data)
+    amount += data
+    puts data
+    puts amount
+  end
+end
+
+
+# Keep it in the bottom of the file
+if __FILE__ == $0
+  main
+end
+
+
